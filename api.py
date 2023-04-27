77c7c2b15bd72ad2fa5a933c5292d5ec82de595b
vanquisher <32311274+Prajwalmithun@users.noreply.github.com>
2023-04-26 22:30:40 -0600

    add another get method to api.py

--- api.py
+++ api.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/home")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/all")
+async def root():
+    return {"message": "Displaying all items in the inventory"}
